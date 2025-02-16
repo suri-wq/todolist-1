b4029a4d03175d331e30a31432795ad852896dd7
yiseul <nisuri@knou.ac.kr>
2025-02-16 17:43:51 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let inputTask = document.getElementById("input-task");
+let addTaskBtn = document.getElementById("add-task");
+task = [];
+
+addTaskBtn.addEventListener("click",addTask)
+
+
+function addTask(){
+    let taskValue = inputTask.value
+
+    task.push(taskValue)
+
+    console.log(task)
+    render();
+}
+
+
+function render(){
+    let resultHTML = "";
+    for (i=0;i<task.length;i++){
+        resultHTML += `<div class="task">
+            <div>${task[i]}</div>
+            <div>
+                <button>Check</button>
+                <button>Delete</button>
+            </div>
+        </div>`
+    }
+
+document.getElementById("task-board").innerHTML=resultHTML;
+}
