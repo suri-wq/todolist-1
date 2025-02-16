8dfdc50a4eac50c69d1faf3100b15de512891eba
yiseul <nisuri@knou.ac.kr>
2025-02-16 18:38:57 +0900

    assignment 2 prep

--- main.js
+++ main.js
@@ -1,31 +1,70 @@
 let inputTask = document.getElementById("input-task");
 let addTaskBtn = document.getElementById("add-task");
-task = [];
+taskList = [];
 
 addTaskBtn.addEventListener("click",addTask)
 
 
 function addTask(){
-    let taskValue = inputTask.value
+    let task = {
+        id : randomIDgenerate(),
+        taskValue : inputTask.value,
+        isComplete : false
+    }
+    taskList.push(task)
 
-    task.push(taskValue)
-
-    console.log(task)
+    console.log("taskValue",task.taskValue)
+    console.log("addTask(), taskList", taskList)
     render();
 }
 
 
 function render(){
     let resultHTML = "";
-    for (i=0;i<task.length;i++){
-        resultHTML += `<div class="task">
-            <div>${task[i]}</div>
+    for (i=0;i<taskList.length;i++){
+        if (taskList[i].isComplete == true){
+            resultHTML += `<div class="task task-done">
+            <div class="task-item">${taskList[i].taskValue}</div>
             <div>
-                <button>Check</button>
-                <button>Delete</button>
+                <button onclick="completeTask('${taskList[i].id}')"><i class="fa-solid fa-repeat"></i></button>
+                <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-regular fa-trash-can"></i></button>
             </div>
         </div>`
+        } else {
+            resultHTML += `<div class="task">
+            <div>${taskList[i].taskValue}</div>
+            <div>
+                <button onclick="completeTask('${taskList[i].id}')"><i class="fa-regular fa-circle-check"></i></button>
+                <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-regular fa-trash-can"></i></button>
+            </div>
+        </div>`
+        }
+
     }
 
 document.getElementById("task-board").innerHTML=resultHTML;
+}
+
+function completeTask(id){
+    for(i=0;i<taskList.length;i++){
+        if (taskList[i].id==id){
+            taskList[i].isComplete = !taskList[i].isComplete;
+        }
+    }
+    console.log("completeTask()",taskList)
+    render();
+}
+
+function deleteTask(id){
+    for(i=0;i<taskList.length;i++){
+        if (taskList[i].id==id){
+            taskList.splice(i,1)
+        }
+    }
+    render();
+}
+
+
+function randomIDgenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
